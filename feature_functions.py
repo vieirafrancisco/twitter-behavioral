ceca00dedb8537997919ae63596d09ed92f730c0
Francisco <fvcneto.master98@gmail.com>
2019-04-23 18:02:09 -0300

    add a module to create features creation functions

---
+++ feature_functions.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+
+import numpy as np
+
+
+# return the string in datetime type
+def string_to_date(string):
+    temp_string = ' '.join(string.split(' ')[1:])
+    temp_string = string.replace("+", "")
+    temp_string = string.replace("0000", "")
+    temp_string = datetime.strptime(temp_string, '%b %d %H:%M:%S  %Y')
+
+    return temp_string
+
+
+def get_intervals(dates):
+    intervals_list = []
+
+    for idx, curr_date in enumerate(dates):
+        if curr_date != dates[-1]:
+            interval = curr_date - dates[idx+1]
+            intervals_list.append(interval.total_seconds())
+
+    lim_sup, lim_inf = limit_iqr(intervals_list)
+    return eliminate_outliers((lim_sup, lim_inf), intervals_list)
+
+
+def verify_day(date):
+    date_day = date.split(' ', 1)[0]
+    if date_day == 'Mon':
+        return 0
+    elif date_day == "Tue":
+        return 1
+    elif date_day == "Wed":
+        return 2
+    elif date_day == "Thu":
+        return 3
+    elif date_day == "Fri":
+        return 4
+    elif date_day == "Sat":
+        return 5
+    elif date_day == "Sun":
+        return 6
+
+
+def verify_hashtags(instance):
+    for hashtag in instance:
+        if hashtag["text"] != '':
+            return 1
+    return 0
+
+
+def number_hashtags(instance):
+    number = 0
+    for hashtag in instance:
+        if hashtag["text"] != '':
+            number += 1
+    return number
+
+
+def limit_iqr(values):
+    values.sort()
+    quartile_1, quartile_3 = np.percentile(values, [25, 75])
+    iqr = quartile_3 - quartile_1
+    lim_sup = quartile_3 + (iqr * 1.5)
+    lim_inf = quartile_1 - (iqr * 1.5)
+
+    return lim_sup, lim_inf
+
+
+def eliminate_outliers(limites, values):
+    without_outliers = []
+    lim_sup, lim_inf = limites
+    for value in values:
+        if(value <= lim_sup and value >= lim_inf):
+            without_outliers.append(value)
+    return without_outliers
